ef1d89d9cd4913aaf85ed99eadce3551eaeec8d6
Lukas Schabler <lukas.schabler@student.tugraz.at>
2017-07-09 23:01:28 +0200

    modified BoidsScript to work for different resolutions, animations script, includet ants

---
+++ Tagorithms/Assets/AnimationScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour {
+
+    public Sprite[] frames;
+    public int fps;
+
+    private int fps_count = 0;
+    private int index = 0;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        fps_count++;
+        if (fps_count < fps)
+            return;
+        fps_count = 0;
+
+        index++;
+        if (index >= frames.Length)
+            index = 0;
+
+        this.GetComponentInParent<SpriteRenderer>().sprite = frames[index];
+
+    }
+}
