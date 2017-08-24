9eb107ad36b5c42a0574101573e88b8b37e2223d
Lukas Schabler <lukas.schabler@student.tugraz.at>
2017-08-24 17:14:18 -0400

    mturk

--- Tagorithms/Assets/Scripts/ShowMTurkNumber.cs
+++ Tagorithms/Assets/Scripts/ShowMTurkNumber.cs
@@ -8,14 +8,13 @@ public class ShowMTurkNumber : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		gameObject.GetComponent<Text>().text =	GerateMTurkNumber.getMTurkNumber ().ToString();
+		//gameObject.GetComponent<Text>().text =	GerateMTurkNumber.getMTurkNumber ().ToString();
 
 		Debug.Log (GerateMTurkNumber.getMTurkNumber ().ToString ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GetComponent<Text> ().text = "";
-		Debug.Log (GetComponent<Text> ().text);
+		GetComponent<InputField> ().text = GerateMTurkNumber.getMTurkNumber ().ToString();
 	}
 };
