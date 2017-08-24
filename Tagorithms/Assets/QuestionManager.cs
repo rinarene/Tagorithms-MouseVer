e3e0e195540f86918f83d4b1bfa52495f315f7b3
Lukas Schabler <lukas.schabler@student.tugraz.at>
2017-08-24 11:15:35 -0400

    Added Question Scene

---
+++ Tagorithms/Assets/QuestionManager.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+
+public class QuestionManager : MonoBehaviour {
+
+	struct Question{
+		public Question(uint number_, string text_)
+		{
+			number = number_;
+			text = text_;
+			rating = -1;
+		}
+
+		public uint number;
+		public string text;
+		public int rating;
+	};
+
+	private int activeQuestion = 0;
+
+	private GameObject[] radioButtons;
+
+	private Text questionText;
+
+	private List<Question> allquestions = new List<Question>();
+
+	private Question[] questions;
+
+	private GameObject confirmButton;
+
+	// Use this for initialization
+	void Start () {
+		radioButtons = GameObject.FindGameObjectsWithTag ("RadioButton");
+
+		questionText = GameObject.FindGameObjectWithTag ("Question").GetComponent<Text>();
+
+		confirmButton = GameObject.FindGameObjectWithTag ("ConfirmButton");
+
+		allquestions.Add (new Question (1, "The level was fun"));
+		allquestions.Add (new Question (2, "The level was difficult"));
+		allquestions.Add (new Question (3, "The level was easy"));
+		allquestions.Add (new Question (4, "The enemies acted \"tricky\" or \"strategic\"."));
+		allquestions.Add (new Question (5, "The enemies were predictable"));
+
+
+		// Todo shuffle
+		questions = (Question[])allquestions.ToArray ();
+
+		questionText.text = questions[activeQuestion].text;
+
+		UncheckAll ();
+
+		disableConfirmButton ();
+	}
+
+	void disableConfirmButton()
+	{
+		confirmButton.SetActive (false);
+	}
+
+	void EnableConfirmButton()
+	{
+		confirmButton.SetActive (true);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void UncheckAll()
+	{
+		foreach(GameObject r in radioButtons)
+		{
+			r.GetComponentInChildren<Image> ().gameObject.SetActive (false);
+		}
+	}
+
+	public void Confirm()
+	{
+		UncheckAll ();
+		activeQuestion++;
+		disableConfirmButton ();
+		if (activeQuestion < questions.Length) 
+		{
+			questionText.text = questions[activeQuestion].text;
+		}
+
+	}
+
+	public void ClickButton1()
+	{
+		UncheckAll ();
+		questions[activeQuestion].rating = 1;
+		EnableConfirmButton ();
+	}
+
+	public void ClickButton2()
+	{
+		UncheckAll ();
+		questions[activeQuestion].rating = 2;
+		EnableConfirmButton ();
+	}
+
+	public void ClickButton3()
+	{
+		UncheckAll ();
+		questions[activeQuestion].rating = 3;
+		EnableConfirmButton ();
+	}
+
+	public void ClickButton4()
+	{
+		UncheckAll ();
+		questions[activeQuestion].rating = 4;
+		EnableConfirmButton ();
+	}
+
+	public void ClickButton5()
+	{
+		UncheckAll ();
+		questions [activeQuestion].rating = 5;
+		EnableConfirmButton ();
+	}
+
+	public void ClickButton6()
+	{
+		UncheckAll ();
+		questions[activeQuestion].rating = 6;
+		EnableConfirmButton ();
+	}
+
+	public void ClickButton7()
+	{
+		UncheckAll ();
+		questions[activeQuestion].rating = 7;
+		EnableConfirmButton ();
+	}
+}
