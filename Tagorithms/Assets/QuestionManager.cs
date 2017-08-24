f5a777944286db5692ed9136572bc540f91679a5
Lukas Schabler <lukas.schabler@student.tugraz.at>
2017-08-24 14:35:20 -0400

    Questions Randomized

--- Tagorithms/Assets/QuestionManager.cs
+++ Tagorithms/Assets/QuestionManager.cs
@@ -33,8 +33,17 @@ public class QuestionManager : MonoBehaviour {
 
 	private GameObject confirmButton;
 
+	private mainData data;
+
 	// Use this for initialization
 	void Start () {
+
+		GameObject dataObject = GameObject.FindWithTag("Main");
+		if (dataObject != null)
+			data = dataObject.GetComponent<mainData>();
+		if (data == null)
+			Debug.Log("Cannot find 'mainData' script");
+
 		radioButtons = GameObject.FindGameObjectsWithTag ("RadioButton");
 
 		questionText = GameObject.FindGameObjectWithTag ("Question").GetComponent<Text>();
@@ -47,6 +56,8 @@ public class QuestionManager : MonoBehaviour {
 		allquestions.Add (new Question (4, "The enemies acted \"tricky\" or \"strategic\"."));
 		allquestions.Add (new Question (5, "The enemies were predictable"));
 
+		Shuffle (allquestions);
+
 
 		// Todo shuffle
 		questions = (Question[])allquestions.ToArray ();
@@ -57,7 +68,23 @@ public class QuestionManager : MonoBehaviour {
 
 		disableConfirmButton ();
 	}
+		
+	void Shuffle(List<Question> list)
+	{
+		int n = list.Count;
 
+		System.Random rng = new System.Random ();
+		while (n > 1) {
+			n--;
+			Debug.Log (n);
+			int k = Random.Range(0, n);
+			Debug.Log (k);
+			Question value = list [k];
+			list [k] = list [n];
+			list [n] = value;
+		}
+	}
+		
 	void disableConfirmButton()
 	{
 		confirmButton.SetActive (false);
